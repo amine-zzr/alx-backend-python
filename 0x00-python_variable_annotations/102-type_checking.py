70637316e58351f5b1feeb662c52d9e446890e94
amine-zzr <aminezoukri@gmail.com>
2024-10-12 02:38:40 +0100

    Type Checking

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from typing import List, Sequence
+
+
+def zoom_array(lst: Sequence, factor: int = 2) -> List:
+    """
+    Zooms into a list by repeating each element `factor` times.
+
+    Args:
+        lst (Sequence): The list or sequence of elements to zoom into.
+        factor (int, optional): The factor by which to zoom in. Defaults to 2.
+
+    Returns:
+        List: A list where each element is repeated `factor` times.
+    """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
